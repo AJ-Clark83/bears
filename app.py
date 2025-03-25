7d1938f0b5cd06114da369227d48d36e9d2cf986
AJ-Clark83 <41601397+AJ-Clark83@users.noreply.github.com>
2025-03-25 21:44:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def get_driver():
     options = Options()
     custom_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
     options.add_argument(f'user-agent={custom_user_agent}')
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     return uc.Chrome(options=options)
 
 
